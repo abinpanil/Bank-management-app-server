366fe904c1f5e890de134f0e979ba09a12184ac5
thomasgeorge760 <thomasgeorge760@gmail.com>
2022-07-21 09:44:51 +0530

    bug

--- controllers/admin/customer/validator/getCustomerSchema.js
+++ controllers/admin/customer/validator/getCustomerSchema.js
@@ -4,4 +4,7 @@ export const getCustomerSchema = Joi.object().keys({
     id: Joi.string().messages({
         "string.base":"Id not valid"
     }),
-})
+    bankId: Joi.string().messages({
+        "string.base":"BankId not valid"
+    })
+});
