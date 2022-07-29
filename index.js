d7d596a4b91c4e6dde6d8f05f350ae06f0e2bd57
abinpanil <abinpanil9@gmail.com>
2022-07-29 11:49:48 +0530

    file size increase

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import  express  from "express";
-import dotenv from 'dotenv';
-import routes from './routes/index.js'
+import express from "express";
+import dotenv from "dotenv";
+import routes from "./routes/index.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
-import multer from 'multer';
-import cors from 'cors';
+import multer from "multer";
+import cors from "cors";
 const upload = multer();
-import connectDb from './config/db.js';
-import path from 'path';
-import {fileURLToPath} from 'url';
+import connectDb from "./config/db.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 export const dirname = path.dirname(__filename);
@@ -19,15 +19,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use(routes);
 
 // error Middlewares
-app.use(notFound)
-app.use(errorHandler)
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
